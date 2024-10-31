048a6acc038790b843a56d28fe9a9c5e26302b28
Jody Boelen <jodyboelen@jodyboelenoutlook.onmicrosoft.com>
2024-10-31 14:23:45 +0100

    Adjusted infra for function app, added deployment script.

--- src/dotnet/DocumentCleanUp/ThreadCleanUpFunctionFromCosmos.cs
+++ src/dotnet/DocumentCleanUp/ThreadCleanUpFunctionFromCosmos.cs
@@ -33,7 +33,7 @@ public class ThreadCleanUpFunctionFromCosmos
         Connection = "CosmosDbConnection",
         LeaseContainerName ="%CosmosDbThreadLease%",
         FeedPollDelay = 5000, // this to ensure that when deleting messages from a thread the trigger is not triggered again
-        CreateLeaseContainerIfNotExists = true)] IReadOnlyList<Thread> threads)
+        CreateLeaseContainerIfNotExists = false)] IReadOnlyList<Thread> threads)
     {
         _logger.LogInformation($"CosmosDbTrigger found {threads.Count} threads which are soft-deleted.");
 
